51ca24a7146f45309f2df1eb150e5246c2f7fe5d
saksh2110 <139182976+saksh2110@users.noreply.github.com>
2023-09-20 16:17:53 +1000

    Add files via upload

---
+++ AboutView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <header>
+      <h1>About Me</h1>
+    </header>
+    <main>
+      <section class="about-me">
+        <h2>Who I Am</h2>
+        <p>I am a passionate web developer with a creative mind and a keen eye for detail. My journey into the world of web development started when I built my first website at the age of 16, and I've been hooked ever since. I specialize in front-end development, creating user-friendly and visually appealing web experiences.</p>
+      </section>
+      <section class="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML5 &amp; CSS3</li>
+          <li>JavaScript (ES6+)</li>
+          <li>Vue.js &amp; React</li>
+          <li>Responsive Web Design</li>
+          <li>UI/UX Design</li>
+          <li>Version Control (Git)</li>
+        </ul>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped>
+/* Specific styles for the about section */
+.about-me {
+  background-color: skyblue;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 2rem;
+}
+
+.about-me h2 {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
+
+.about-me p {
+  font-size: 1rem;
+  line-height: 1.5;
+}
+
+.skills ul {
+  list-style-type: disc;
+  padding-left: 1rem;
+}
+
+.skills li {
+  font-size: 1rem;
+  margin-top: 0.5rem;
+}
+</style>
---
+++ HomeView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <header>
+      <h1>Welcome to My Portfolio</h1>
+    </header>
+    <main>
+      <section class="intro">
+        <h2>Hello, I'm Saksham</h2>
+        <p>Welcome to my portfolio website. I'm a web developer passionate about creating user-friendly and visually appealing websites. Explore my projects below.</p>
+      </section>
+      <section class="featured-projects">
+        <h2>Featured Projects</h2>
+        <div class="project-card">
+          <h3>Project 1</h3>
+          <p>This is an imaginary project description.</p>
+        </div>
+        <div class="project-card">
+          <h3>Project 2</h3>
+          <p>Another imaginary project description.</p>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+/* Specific styles for the home page */
+.intro {
+  background-color: pink;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 2rem;
+  text-align: center;
+}
+
+.intro h2 {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
+
+.intro p {
+  font-size: 1rem;
+  line-height: 1.5;
+}
+
+.featured-projects {
+  margin-top: 2rem;
+}
+
+.project-card {
+  background-color: plum;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.project-card img {
+  max-width: 100%;
+  height: auto;
+}
+
+.project-card h3 {
+  font-size: 1.25rem;
+  margin-top: 1rem;
+}
+
+.project-card p {
+  font-size: 1rem;
+  margin-top: 0.5rem;
+}
+
+</style>
+
+
